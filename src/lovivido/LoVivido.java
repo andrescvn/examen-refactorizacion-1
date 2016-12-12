321f225c107f8da010d51b0b003ecfb07e7344ac
acomesanavila <acomesanavila@victoria7.danielcastelao.org>
2016-12-12 08:43:51 +0100

    cambio de nombre en variable nombres dias y horas y fromateo de todo el codigo con la herramienta dada por netbeans

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -8,34 +8,25 @@ import javax.swing.JOptionPane;
 
 public class LoVivido {
 
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
+        int meses, dias, horas;
 
-int m;
-int d;
-int h;
-int a;
+        String nombre = JOptionPane.showInputDialog("Escriba su nombre: ");
+        int edad = Integer.parseInt(JOptionPane.showInputDialog("Escriba su edad: "));
 
-String nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
-int edad = Integer.parseInt(JOptionPane.showInputDialog ("Escriba su edad: "));
- 
+        meses = (edad * 12);
+        dias = (edad * 365);
+        horas = (dias * 24);
 
- 
-m = (a*12);
-d = (a*365);
-h = (d*24);
- 
- JOptionPane.showMessageDialog(null,  "Meses: "  +m,"Numero de meses vividos de " +nombre,JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Meses: " + meses, "Numero de meses vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
 
+        JOptionPane.showMessageDialog(null, "Días: " + dias, "Numero de días vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
 
- JOptionPane.showMessageDialog(null, "Días: " +d,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Horas: " + horas, "Numero de horas vividos de " + nombre, JOptionPane.INFORMATION_MESSAGE);
 
+        System.exit(0);
 
- JOptionPane.showMessageDialog(null, "Horas: " +h,"Numero de horas vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
-
-   System.exit (0);
-
-}
-   
+    }
 
 }
